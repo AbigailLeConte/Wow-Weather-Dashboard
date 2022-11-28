6fcaf338e93353d23b1406f91bf67f7033e7009a
Abigail LeConte <113259320+AbigailLeConte@users.noreply.github.com>
2022-11-27 22:55:35 -0500

    connecting api

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # Wow-Weather-Dashboard
-Challenge 6
+
+## Description
+
+A weather dashboard that will run in the browser and feature dynamically updated HTML and CSS. This website uses the OpenWeather One Call API to retrieve weather data for cities.
+
+## Usage
+
+[Preview of this application!](./assets/WWD-image.png)
+
+
+## How to Contribute
+
+[Connect with me on GitHub!](https://github.com/AbigailLeConte)
---
+++ assets/js/script.js
@@ -0,0 +1,126 @@
+var WowWeatherDashboard = "595331f3d9fc970ad48ad7474e5926a4";
+
+var city;
+
+api.openweathermap.org/data/2.5/weather?q={city}&appid:{WowWeatherDashboard};
+
+var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + WowWeatherDashboard;
+
+fetch(queryURL) 
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    console.log(data);
+  });
+
+  var resultTextEl = document.querySelector('#result-text');
+var resultContentEl = document.querySelector('#result-content');
+var searchFormEl = document.querySelector('#search-form');
+
+function getParams() {
+  var searchParamsArr = document.location.search.split('&');
+  var query = searchParamsArr[0].split('=').pop();
+  var format = searchParamsArr[1].split('=').pop();
+
+  searchApi(query, format);
+}
+
+function printResults(resultObj) {
+  console.log(resultObj);
+
+  // set up `<div>` to hold result content
+  var resultCard = document.createElement('div');
+  resultCard.classList.add('card', 'bg-light', 'text-dark', 'mb-3', 'p-3');
+
+  var resultBody = document.createElement('div');
+  resultBody.classList.add('card-body');
+  resultCard.append(resultBody);
+
+  var titleEl = document.createElement('h3');
+  titleEl.textContent = resultObj.title;
+
+  var bodyContentEl = document.createElement('p');
+  bodyContentEl.innerHTML =
+    '<strong>City:</strong> ' + resultObj.city + '<br/>';
+
+  if (resultObj.subject) {
+    bodyContentEl.innerHTML +=
+      '<strong>Subjects:</strong> ' + resultObj.subject.join(', ') + '<br/>';
+  } else {
+    bodyContentEl.innerHTML +=
+      '<strong>Subjects:</strong> No subject for this entry.';
+  }
+
+  if (resultObj.description) {
+    bodyContentEl.innerHTML +=
+      '<strong>Description:</strong> ' + resultObj.description[0];
+  } else {
+    bodyContentEl.innerHTML +=
+      '<strong>Description:</strong>  No description for this entry.';
+  }
+
+  var linkButtonEl = document.createElement('a');
+  linkButtonEl.textContent = 'Read More';
+  linkButtonEl.setAttribute('href', resultObj.url);
+  linkButtonEl.classList.add('btn', 'btn-dark');
+
+  resultBody.append(titleEl, bodyContentEl, linkButtonEl);
+
+  resultContentEl.append(resultCard);
+}
+
+function searchApi(query, format) {
+  var QueryUrl = 'http://api.openweathermap.org/data/2.5/weather?q=?fo=json';
+
+  if (format) {
+    QueryUrl = 'hhttp://api.openweathermap.org/data/2.5/weather?q=' + format + '/?fo=json';
+  }
+
+  locQueryUrl = locQueryUrl + '&q=' + query;
+
+  fetch(locQueryUrl)
+    .then(function (response) {
+      if (!response.ok) {
+        throw response.json();
+      }
+
+      return response.json();
+    })
+    .then(function (locRes) {
+      resultTextEl.textContent = locRes.search.query;
+
+      console.log(locRes);
+
+      if (!locRes.results.length) {
+        console.log('No results found!');
+        resultContentEl.innerHTML = '<h3>No results found, search again!</h3>';
+      } else {
+        resultContentEl.textContent = '';
+        for (var i = 0; i < locRes.results.length; i++) {
+          printResults(locRes.results[i]);
+        }
+      }
+    })
+    .catch(function (error) {
+      console.error(error);
+    });
+}
+
+function handleSearchFormSubmit(event) {
+  event.preventDefault();
+
+  var searchInputVal = document.querySelector('#search-input').value;
+  var formatInputVal = document.querySelector('#format-input').value;
+
+  if (!searchInputVal) {
+    console.error('You need a search input value!');
+    return;
+  }
+
+  searchApi(searchInputVal, formatInputVal);
+}
+
+searchFormEl.addEventListener('submit', handleSearchFormSubmit);
+
+getParams();
